1ff3adf4d78e6a137a7aeb10f9aea9c0671ce74f
Mikhail Boldyrev <miboldyrev@gmail.com>
2019-02-13 10:11:56 +0300

    class name length padding in counter
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- libs/obj_counter.hpp
+++ libs/obj_counter.hpp
@@ -6,6 +6,8 @@
 
 #include <boost/core/demangle.hpp>
 
+static std::atomic<size_t> obj_counter_longest_class_name(0);
+
 template <typename T>
 struct ObjCounter {
   static std::atomic<size_t> objects_created;
@@ -18,9 +20,12 @@ struct ObjCounter {
 
   static std::string getStats() {
     std::lock_guard<std::mutex> lock(get_stats_mu_);
+    const size_t class_name_padding_length =
+        obj_counter_longest_class_name.load(std::memory_order_relaxed)
+        - class_name_.size();
     std::stringstream ss;
-    ss << class_name_ << ": created "
-       << objects_created.load(std::memory_order_relaxed)
+    ss << class_name_ << ": " << std::string(class_name_padding_length, '.')
+       << " created " << objects_created.load(std::memory_order_relaxed)
        << ", alive :" << objects_alive.load(std::memory_order_relaxed);
     return ss.str();
   }
@@ -43,4 +48,14 @@ std::atomic<size_t> ObjCounter<T>::objects_alive(0);
 template <typename T>
 std::mutex ObjCounter<T>::get_stats_mu_;
 template <typename T>
-std::string ObjCounter<T>::class_name_(boost::core::demangle(typeid(T).name()));
+std::string ObjCounter<T>::class_name_([] {
+  std::string class_name = boost::core::demangle(typeid(T).name());
+  const size_t my_class_name_length = class_name.size();
+  size_t cur_max_length = obj_counter_longest_class_name;
+  while (not std::atomic_compare_exchange_weak(
+      &obj_counter_longest_class_name,
+      &cur_max_length,
+      std::max(my_class_name_length, cur_max_length)))
+    ;
+  return class_name;
+}());
